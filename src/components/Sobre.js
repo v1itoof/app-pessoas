72b355eb87d1c32dcf65a6049fcda67b43c2e693
v1itoof <joaov1c@outlook.com>
2024-04-05 16:57:07 -0300

    Minha api funcionando

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -1,9 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Sobre() {
-    return (
-        <div>
-            <p>Sobre</p>
-        </div>
-    );
+  // Definir o estado para armazenar os dados da API
+  const [pessoas, setPessoas] = useState([]);
+
+  useEffect(() => {
+    // Função assíncrona para buscar os dados da API local
+    async function fetchData() {
+      try {
+        // Fazer a solicitação à API local
+        const response = await axios.get('http://localhost:3003/pessoas/');
+        // Definir os dados recebidos no estado 'pessoas'
+        setPessoas(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    }
+
+    fetchData(); // Chamar a função para buscar os dados da API local
+  }, []); // Usar um array vazio como segundo argumento para que a chamada ocorra apenas uma vez, ao montar o componente
+
+  return (
+    <div>
+      <h1>Pessoas</h1>
+      <ul>
+        {/* Mapear os dados das pessoas e exibi-los em uma lista */}
+        {pessoas.map((pessoa, index) => (
+          <li key={index}>{pessoa.nome}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Sobre;
